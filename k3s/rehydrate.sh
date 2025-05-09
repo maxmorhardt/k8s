1864532a00cd7469579c222ca2cabfc5b5d4f4ae
Max Morhardt <70174942+maxmorhardt@users.noreply.github.com>
2025-05-09 15:39:16 -0400

    upgrade scripts + log rehydrate

--- k3s/rehydrate.sh
+++ k3s/rehydrate.sh
@@ -2,11 +2,15 @@
 
 ### To run: sudo su && sh /usr/local/bin/rehydrate.sh ###
 
+echo Running k3s kill all
 /bin/bash /usr/local/bin/k3s-killall.sh
 
+echo Upgrading apt packages
 apt-get update
 apt-get upgrade -y
 
+echo Removing backups
 rm -rf /boot/firmware/*.bak
 
+echo Rebooting
 reboot
