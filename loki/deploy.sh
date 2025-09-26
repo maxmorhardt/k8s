700269c6835d85ec47d54a3fd54e5b1107114b6c
Max Morhardt <70174942+maxmorhardt@users.noreply.github.com>
2025-09-26 11:21:03 -0400

    loki improvements

--- loki/deploy.sh
+++ loki/deploy.sh
@@ -1,4 +1,4 @@
 helm repo add grafana https://grafana.github.io/helm-charts 
 helm repo update
-helm upgrade loki grafana/loki --values values.yaml --install --atomic --debug --history-max=3 --namespace maxstash-global
 
+helm upgrade loki grafana/loki --values values.yaml --install --atomic --debug --history-max=3 --namespace maxstash-global
