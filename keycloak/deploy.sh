d473fa46ac7b5815589db95cca013b6a45ceca43
Max Morhardt <70174942+maxmorhardt@users.noreply.github.com>
2025-12-29 14:30:22 -0500

    keycloak bitnami chart

--- keycloak/deploy.sh
+++ keycloak/deploy.sh
@@ -1,7 +1,9 @@
-set -x
-
-helm package helm
-
-helm upgrade keycloak keycloak-1.0.0.tgz --install --atomic --debug --history-max=3 --namespace maxstash-global --timeout 15m0s
-
-rm keycloak-1.0.0.tgz
+helm upgrade keycloak oci://registry-1.docker.io/bitnamicharts/keycloak \
+  --version 23.2.1 \
+  --install \
+  --atomic \
+  --debug \
+  --history-max=3 \
+  --namespace maxstash-global \
+  --timeout 15m0s \
+  --values values.yaml
