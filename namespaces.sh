bc115904c9b306c3c8007da9b5adafcb54009b20
Max Morhardt <70174942+maxmorhardt@users.noreply.github.com>
2026-01-01 08:34:36 -0500

    safety commit

--- namespaces.sh
+++ namespaces.sh
@@ -2,7 +2,7 @@ kubectl create namespace cattle-system
 
 kubectl create namespace db
 kubectl create namespace jenkins
-kubectl create namespace zitadel
+kubectl create namespace logto
 
 kubectl create namespace monitoring
 
