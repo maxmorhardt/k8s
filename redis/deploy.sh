3725fc46a6fe8b2d03650cafdab01569318505ba
Max Morhardt <70174942+maxmorhardt@users.noreply.github.com>
2026-01-12 13:38:13 -0500

    only use replicas w sent

--- redis/deploy.sh
+++ redis/deploy.sh
@@ -1,7 +1,11 @@
 helm repo add bitnami https://charts.bitnami.com/bitnami
 helm repo update
 
-helm upgrade --install redis bitnami/redis \
-  --namespace redis \
-  --create-namespace \
-  -f values.yaml
+helm upgrade redis bitnami/redis \
+	--values values.yaml \
+	--install \
+	--rollback-on-failure \
+	--wait \
+	--debug \
+	--history-max=3 \
+	--namespace redis
