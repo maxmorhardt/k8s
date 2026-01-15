4d17e20daa534ccc6d7a209bb8f457beceafd66a
Max Morhardt <70174942+maxmorhardt@users.noreply.github.com>
2026-01-15 12:32:32 -0500

    discord alerts + authentik probe adjustments

--- prometheus-stack/deploy.sh
+++ prometheus-stack/deploy.sh
@@ -1,8 +1,18 @@
+#!/bin/bash
+
+if [ -z "$1" ]; then
+  echo "Usage: ./deploy.sh <discord-webhook-url>"
+  exit 1
+fi
+
+DISCORD_WEBHOOK="$1"
+
 helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
 helm repo update
 
 helm upgrade kube-prometheus-stack prometheus-community/kube-prometheus-stack \
 	--values values.yaml \
+	--set alertmanager.config.receivers[2].discord_configs[0].webhook_url="$DISCORD_WEBHOOK" \
 	--install \
 	--rollback-on-failure \
 	--wait \
